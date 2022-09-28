4456d70359f8c63aa9711494fe99236d99679f62
Alejandro Medina <alejandro.medina@universae.com>
2022-09-28 15:00:51 +0200

    logos metidos

--- src/components/skills.js
+++ src/components/skills.js
@@ -2,21 +2,40 @@ import React from "react";
 import { Parallax } from "react-parallax";
 import image4 from "../imagenes/image9.jpg";
 import './fullstack.css'
+import logo1 from "../logos/Bootstrap_logo.png";
+import logo2 from "../logos/css.png";
+import logo3 from "../logos/express-js.png";
+import logo4 from "../logos/framer-motion.png";
+import logo5 from "../logos/HTML.png";
+import logo6 from "../logos/JavaScript.png";
+import logo7 from "../logos/MaterialUI.png";
+import logo8 from "../logos/mongodb.png";
+import logo9 from "../logos/nodejs.png";
+import logo10 from "../logos/postgresql.png";
+import logo11 from "../logos/React.png";
+import logo12 from "../logos/redux.png";
+import logo13 from "../logos/sequelize.png";
+import logo14 from "../logos/TypeScript.png";
+import logo15 from "../logos/webpack.png";
+
 
 
 export default function Skills(){
+    const skill=['JavaScript','NodeJs','HTML5' ]
     return(
         <div id='Skills'>
             <Parallax className="imagefull" bgImage={image4} strength={100}>
                 <div className="containerfull">
-                    <h1>FULL STACK</h1>
-                    <div className="text" >Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                    when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                    It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-                    It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-                    and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                    </div>
+                    <h1>FrontEnd</h1>
+                        <ul>
+                            <li ><img src='' alt=''></img>Node Js</li>
+                        </ul>
+                </div>
+                <div className="containerfull">
+                    <h1>BackEnd</h1>
+                        <ul>
+                            <li ><img className='' src={logo9} alt='logo'></img>Node Js</li>
+                        </ul>
                 </div>
             </Parallax>
         </div>
